0d4c4da5d61101930c77b1d2d47613a2392535d9
Juan Casas <imhouses@ciencias.unam.mx>
2018-01-07 00:15:25 -0600

    Comment improvement.

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
+/* SERVER FILE FOR BOTH ANGULAR APP AND API. */
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
 
-// API file for interacting with MongoDB
+/* API file for interacting with MongoDB. */
 const api = require('./server/routes/api');
 
-// Parsers
+/* Parsers. */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-// Angular DIST output folder
+/* Angular DIST output folder, this folder is used to redirect to our Angular app. */
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// API location
+/* API location. */
 app.use('/api', api);
 
-// Send all other requests to the Angular app
+/* Send all other requests to the Angular app. */
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-//Set Port
+/* PORT setup. */
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
